00127f17686437eb899686fb19f97613f3d6831a
SL4T3R <shinken4200@gmail.com>
2025-07-10 12:16:43 -0400

    7/10/25

--- Assets/Geo_Quest/Scenes/geo.cs
+++ Assets/Geo_Quest/Scenes/geo.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
-    // Variables
+// Variables
 public class geo : MonoBehaviour
 {
     private int varOne = 1;
 
     private string Shinken = "Wassup";
 
+    private Rigidbody2D rb;
+    public int speed = 3;
+
     // Start is called before the first frame update
     void Start()
     {
         int varTwo = 2;
         Debug.Log("Hello World!");
-        string Kenshin = "Dude";
-        Debug.Log(Shinken+Kenshin);
+        string Kenshin = " dude";
+        Debug.Log(Shinken + Kenshin);
 
+        rb = GetComponent<Rigidbody2D>();
     }
-    int var = 3;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var);
-        var++;
-    }
-}
+
+        
+            float xSpeed = Input.GetAxis("Horizontal");
+            Debug.Log(xSpeed);
+
+            rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
+        }
+        
+    private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("Hit");
+        }
+}    
