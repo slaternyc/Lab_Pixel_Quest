05d4295299779915d091ac3603a4ec65ffb73e9d
SL4T3R <shinken4200@gmail.com>
2025-07-21 13:27:10 -0400

    7/21/25 Morning

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -1,6 +1,29 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+
+    public float speed = 5;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(InputX);
+        _ySpeed = Input.GetAxis(InputY);
+
+        _rigidbody2D.velocity = new Vector2(_xSpeed, _ySpeed) * speed;
+    }
 }
+ 
--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,47 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public GameObject preFab2;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+            
+            
+        if(Input.GetKeyUp(KeyCode.Mouse0) && _canShoot)
+        { 
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+    
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab2, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+
+        }
+    }
 }
