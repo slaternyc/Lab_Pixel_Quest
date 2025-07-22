61e2cceb08954d51f9b7f02452e1d24f1c0c11d8
SL4T3R <shinken4200@gmail.com>
2025-07-22 12:17:41 -0400

    7/22/25 Morning

---
+++ Assets/Pixel_Quest/Scripts/LevelGoal.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour
+{
+    public string nextLevel = "Level_2";
+}
+
--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
+    public Transform respawnPoint;
     public string nextLevel = "Level 2";
+    private int _coinCounter = 0;
+    private int _health = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -15,24 +18,53 @@ public class PlayerStats : MonoBehaviour
 
 
   
-         private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
         {
-            case "Death":
-                {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
-                }
             case "Finish":
 
                 {
+                    string nextLevel = collision.transform.GetComponent<LevelGoal>().nextLevel;
                     SceneManager.LoadScene(nextLevel);
                     break;
                 }
+            case "Health":
+                {
+                    if (_health < 3)
+                    {
+                        _health++;
+                        Destroy(collision.gameObject);
+                        
+                    }
+                    break;
+                }
+            case "Death":
+                {
+                    _health--;
+                    if (_health <= 0)
+                    {
+                        string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thisLevel);
+                    }
+                    else
+                    {
+                        transform.position = respawnPoint.position;
+                    }
+                    break;
+                }
+            case "Respawn":
+                {
+                    respawnPoint.position = collision.transform.Find("Point").position;
+                    break;
+                }
+            case "Coin":
+            {
+                 _coinCounter++;
+                 Destroy(collision.gameObject);
+                 break;
+            }
+
         }
-
-
     }
 }
