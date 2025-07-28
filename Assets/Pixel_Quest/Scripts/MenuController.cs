d1819f9c95dcd8d5c6a3af85e5d49ff95cdff6a4
SL4T3R <shinken4200@gmail.com>
2025-07-28 14:36:00 -0400

    7/28/25

--- Assets/Pixel_Quest/Scripts/MenuController.cs
+++ Assets/Pixel_Quest/Scripts/MenuController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public string startScene;
+
     // Start is called before the first frame update
-    void Start()
+    public void LoadLevel()
     {
-        
+        SceneManager.LoadScene(startScene);
     }
 
     // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
